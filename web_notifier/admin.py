fc80087e8d2d6c82c7974a05aa0f7ed2778990eb
IbnuAmin <mihfazhillah@gmail.com>
2018-06-27 22:55:16 +0700

    Add admin boilerplate

---
+++ web_notifier/admin.py
@@ -0,0 +1,3 @@
+from flask_admin import Admin
+
+admin = Admin(name='Notifier Admin', template_mode='bootstrap3')
--- web_notifier/create_app.py
+++ web_notifier/create_app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from .admin import admin
 
 def create_app():
     app = Flask(__name__)
+    admin.init_app(app)
 
     @app.route("/")
     def index():
