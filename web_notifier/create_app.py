854f33c3a0dfaca86b4788b9938cfcdd746a4d5c
IbnuAmin <mihfazhillah@gmail.com>
2018-06-27 23:24:47 +0700

    integrate app with mongodb

--- web_notifier/create_app.py
+++ web_notifier/create_app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from .admin import admin
+from .models import db
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_pyfile("config.py")
     admin.init_app(app)
+    db.init_app(app)
 
     @app.route("/")
     def index():
