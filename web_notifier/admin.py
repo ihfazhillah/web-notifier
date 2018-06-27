0ab77e3a09ed714e3b5deb6ab3b56613dac1dac9
IbnuAmin <mihfazhillah@gmail.com>
2018-06-27 23:24:28 +0700

    Integrate admin with models

--- web_notifier/admin.py
+++ web_notifier/admin.py
@@ -1,3 +1,8 @@
 from flask_admin import Admin
+from flask_admin.contrib.mongoengine import ModelView
+
+from web_notifier.models import Url, Item
 
 admin = Admin(name='Notifier Admin', template_mode='bootstrap3')
+admin.add_view(ModelView(Url))
+admin.add_view(ModelView(Item))
